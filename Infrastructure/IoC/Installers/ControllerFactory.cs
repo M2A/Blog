b1bbf7fc9dc355353cc87fb7eab8e7722969e033
M2A <azadmojtaba@gmail.com>
2013-01-09 13:12:02 +0330

    after reset

---
+++ Infrastructure/IoC/Installers/ControllerFactory.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Yara.Blog.Infrastructure.IoC.Installers
+{
+    class ControllerFactory
+    {
+    }
+}
---
+++ Infrastructure/IoC/Installers/ControllerFactoryInstaller.cs
@@ -0,0 +1,20 @@
+﻿using Castle.MicroKernel.Registration;
+using Castle.MicroKernel.SubSystems.Configuration;
+using Castle.Windsor;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace Yara.Blog.Infrastructure.IoC.Installers
+{
+    public class ControllerFactoryInstaller : IWindsorInstaller
+    {
+        public void Install(IWindsorContainer container, IConfigurationStore store)
+        {
+            ControllerBuilder.Current.SetControllerFactory(new ControllerFactory(container));
+        }
+    }
+}
